6c7b4f62c21f34f2e69f501ccd52cc324075db60
Онищенко Арина <onishenkoarinka@yandex.ru>
2024-09-27 23:51:54 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title›Онищенко Арина Сергеевна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ОБ, Лабораторная работа 1
+        </header»
+
+        <h1>web-сервер на flasks</h1>
+
+        <footer>
+            &copy; Онищенко Арина, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
