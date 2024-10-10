5492e9d6b83f7664b73ec1c4649661398e37c1f1
Онищенко Арина <onishenkoarinka@yandex.ru>
2024-10-10 21:34:25 +0700

    Имя студента

--- app.py
+++ app.py
@@ -184,4 +184,5 @@ def add_flower(name):
 '''
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Онищенко Арина'
+    return render_template('example.html', name=name)
