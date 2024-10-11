35905b4defcecc6204091c969c71daecfc8c25c8
Онищенко Арина <onishenkoarinka@yandex.ru>
2024-10-11 14:56:59 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -197,3 +197,7 @@ def example():
     ]
     return render_template('example.html', name=name, course=course, laba=laba, 
                            group=group, fruits=fruits)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
