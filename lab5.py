c2c2b4c6d208fd69c55adb93b2a4a1cd51629151
Онищенко Арина <onishenkoarinka@yandex.ru>
2024-11-21 22:36:30 +0700

    Вывод статей

--- lab5.py
+++ lab5.py
@@ -78,7 +78,20 @@ def register():
 
 @lab5.route('/lab5/list')
 def list():
-    return render_template('lab5/list.html')
+    login = session.get('login')
+    if not login: 
+        return redirect('lab5/login') 
+    
+    conn, cur = db_connect()
+
+    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
+    user_id = cur.fetchone()["id"]
+
+    cur.execute(f"SELECT * FROM articles WHERE user_id='{user_id}';")
+    articles = cur.fetchall()
+
+    db_close(conn, cur)
+    return render_template ('/lab5/articles.html', articles=articles)
 @lab5.route('/lab5/create', methods = ['GET', 'POST'])
 def create():
     login = session.get('login')
@@ -95,3 +108,4 @@ def create():
     cur.execute(f"INSERT INTO articles (user_id, tittle, article_text) VALUES ('{user_id}', '{tittle}', '{article_text}')")
     db_close(conn, cur)
     return redirect ('/lab5')
+
