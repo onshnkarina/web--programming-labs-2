65e7fb0646ffab8a3e3681a5ca880bfb63b3cbb7
Онищенко Арина <onishenkoarinka@yandex.ru>
2024-12-06 18:22:25 +0700

    Доп задание

--- lab6.py
+++ lab6.py
@@ -1,9 +1,31 @@
-from flask import Blueprint, render_template, redirect, request, session, current_app
+from flask import Blueprint, render_template, request, session, current_app
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from os import path
+import sqlite3
 lab6 = Blueprint('lab6', __name__)
 
-offices =[]
-for i in range (1, 11):
-    offices.append({"number": i, "tenant": "", "price": 1000})
+def db_connect():
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+            host='127.0.0.1',
+            database='arina_onishchenko_knowledge_base',
+            user='arina_onishchenko_knowledge_base',
+            password='12345'
+        )
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "database.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
 
 @lab6.route('/lab6/')
 def lab():
@@ -13,8 +35,15 @@ def lab():
 def api():
     data = request.json
     id = data['id']
+
+    conn, cur = db_connect()
+
     if data['method'] == 'info':
-        total_cost = sum(office['price'] for office in offices if office['tenant'])
+        cur.execute("SELECT * FROM offices")
+        offices = cur.fetchall()
+        cur.execute("SELECT SUM(price) AS total_cost FROM offices WHERE tenant IS NOT NULL")
+        total_cost = cur.fetchone()['total_cost'] or 0
+        db_close(conn, cur)
         return {
             'jsonrpc': '2.0',
             'result': {
@@ -24,53 +53,75 @@ def api():
             'id': id
         }
     login = session.get('login')
-    if not login: 
+    if not login:
+        db_close(conn, cur) 
         return {
-        'jsonrpc': '2.0', 
-        'error': {
-            'code': 1,
-            'message': 'Unauthorized'
-        },
-        'id': id
-    }
+            'jsonrpc': '2.0',
+            'error': {
+                'code': 1,
+                'message': 'Unauthorized'
+            },
+            'id': id
+        }
     if data['method'] == 'booking':
         office_number = data['params']
-        for office in offices:
-            if office['number'] == office_number:
-                if office['tenant'] != '':
-                    return {
-                        'jsonrpc': '2.0', 
-                        'error': {
-                            'code': 2,
-                            'message': 'Already'
-                        },
-                        'id': id
-                    }
-                office['tenant'] = login
-                return {
-                'jsonrpc': '2.0', 
-                'result': 'success',
+        if current_app.config['DB_TYPE'] == 'postgres':
+            cur.execute("SELECT tenant FROM offices WHERE number = %s", (office_number,))
+        else:
+            cur.execute("SELECT tenant FROM offices WHERE number = ?", (office_number,))
+        
+        office = cur.fetchone()
+        if office and office['tenant']:
+            db_close(conn, cur)
+            return {
+                'jsonrpc': '2.0',
+                'error': {
+                    'code': 2,
+                    'message': 'Already'
+                },
                 'id': id
-                }
+            }
+        if current_app.config['DB_TYPE'] == 'postgres':
+            cur.execute("UPDATE offices SET tenant = %s WHERE number = %s", (login, office_number))
+        else:
+            cur.execute("UPDATE offices SET tenant = ? WHERE number = ?", (login, office_number))
+        
+        db_close(conn, cur)
+        return {
+            'jsonrpc': '2.0',
+            'result': 'success',
+            'id': id
+        }
+        
     if data['method'] == 'cancellation':
         office_number = data['params']
-        for office in offices:
-            if office['number'] == office_number:
-                if office['tenant'] != login:
-                    return {
-                        'jsonrpc': '2.0',
-                        'error': {
-                            'code': 3,
-                            'message': 'Forbidden'
-                        },
-                        'id': id
-                    }
-                office['tenant'] = ''
-                return {
-                    'jsonrpc': '2.0',
-                    'result': 'success',
-                    'id': id
-                }
+        if current_app.config['DB_TYPE'] == 'postgres':
+            cur.execute("SELECT tenant FROM offices WHERE number = %s", (office_number,))
+        else:
+            cur.execute("SELECT tenant FROM offices WHERE number = ?", (office_number,))
+        
+        office = cur.fetchone()
+        if office and office['tenant'] != login:
+            db_close(conn, cur)
+            return {
+                'jsonrpc': '2.0',
+                'error': {
+                    'code': 3,
+                    'message': 'Forbidden'
+                },
+                'id': id
+            }
+        if current_app.config['DB_TYPE'] == 'postgres':
+            cur.execute("UPDATE offices SET tenant = NULL WHERE number = %s", (office_number,))
+        else:
+            cur.execute("UPDATE offices SET tenant = NULL WHERE number = ?", (office_number,))
+        
+        db_close(conn, cur)
+        return {
+            'jsonrpc': '2.0',
+            'result': 'success',
+            'id': id
+        }
     return {
         'jsonrpc': '2.0', 
         'error': {
