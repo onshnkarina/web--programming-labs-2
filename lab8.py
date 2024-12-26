4d114c7c241ccbf75ee8ee2c2bcd736226e51519
Онищенко Арина <onishenkoarinka@yandex.ru>
2024-12-26 16:17:57 +0700

    САмостоятельное задание

--- lab8.py
+++ lab8.py
@@ -30,6 +30,7 @@ def register():
     new_user = users(login=login_form, password=password_hash)
     db.session.add(new_user)
     db.session.commit()
+    login_user(new_user, remember=False)
     return redirect('/lab8/')
 
 @lab8.route('/lab8/login/', methods=['GET', 'POST'])
@@ -39,27 +40,56 @@ def login():
     
     login_form = request.form.get('login')
     password_form = request.form.get('password')
-    user = users.query.filter_by(login = login_form).first()
+    remember = request.form.get('remember') == 'on'
     if not login_form:
         return render_template('lab8/login.html',
                                error='Имя пользователя не должно быть пустым')
     if not password_form:
         return render_template('lab8/login.html',
                                error='Пароль не должен быть пустым')
-    if user:
-        if check_password_hash(user.password, password_form):
-            login_user(user, remember=False)
-            return redirect('/lab8/')
+    user = users.query.filter_by(login=login_form).first()
+    if user and check_password_hash(user.password, password_form):
+        login_user(user, remember=remember)
+        return redirect('/lab8/')
         
     return render_template('/lab8/login.html',
-                           error = 'Ошибка входа: логин и/или пароль неверны')
+                           error='Ошибка входа: логин и/или пароль неверны')
 @lab8.route('/lab8/articles/')
 @login_required
 def article_list():
-    return 'Спиоск статей'
+    user_articles = articles.query.filter_by(login_id=current_user.id).all()
+    return render_template('lab8/articles.html', articles=user_articles)
 
 @lab8.route('/lab8/logout')
 @login_required
 def logout():
     logout_user()
     return redirect('/lab8/')
+@lab8.route('/lab8/create', methods=['GET', 'POST'])
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        article_text = request.form.get('article_text')
+        new_article = articles(login_id=current_user.id, title=title, article_text=article_text)
+        db.session.add(new_article)
+        db.session.commit()
+        return redirect('/lab8/articles')
+    return render_template('lab8/create.html')
+@lab8.route('/lab8/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    article = articles.query.get_or_404(id)
+    if request.method == 'POST':
+        article.title = request.form.get('title')
+        article.article_text = request.form.get('article_text')
+        db.session.commit()
+        return redirect('/lab8/articles')
+    return render_template('lab8/edit.html', article=article)
+@lab8.route('/lab8/delete/<int:id>', methods=['POST'])
+@login_required
+def delete(id):
+    article = articles.query.get_or_404(id)
+    db.session.delete(article)
+    db.session.commit()
+    return redirect('/lab8/articles')
